0971e59d8ecb24d2bbd8ceb410d868fcb99beda3
t5krishn <tk95@live.ca>
2019-06-24 18:32:13 +0000

    Added tail.js after linting

---
+++ tail.js
@@ -0,0 +1,27 @@
+const  assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u274c \u274c \u274c Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const tail = function(arr) {
+
+  let [, ...end] = arr;
+
+  return end;
+
+};
+
+const result = tail([1,2,3,4]);
+assertEqual(result.length, 3);
+assertEqual(result[0], 2);
+assertEqual(result[1], 3);
+assertEqual(result[2], 4);
+
+const sameArrCheck = [1,2,3,4,5];
+tail(sameArrCheck);
+assertEqual(sameArrCheck.length, 5);
