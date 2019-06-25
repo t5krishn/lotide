9a82d04674995f3f50421ce62523d8c5c1f7c4f2
t5krishn <tk95@live.ca>
2019-06-25 17:56:56 +0000

    Added comment for test case

--- tail.js
+++ tail.js
@@ -22,6 +22,7 @@ assertEqual(result[0], 2);
 assertEqual(result[1], 3);
 assertEqual(result[2], 4);
 
+// Checks for modification of array
 const sameArrCheck = [1,2,3,4,5];
 tail(sameArrCheck);
 assertEqual(sameArrCheck.length, 5);
