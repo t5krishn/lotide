26d850fd568f31e284e376034d7b27aad5dbc163
t5krishn <tk95@live.ca>
2019-06-25 19:40:25 +0000

    Added recursive feature to flatten.js

--- flatten.js
+++ flatten.js
@@ -24,18 +24,20 @@ const flatten = function(arr) {
   let result = [];
 
   for (let index = 0; index < arr.length; index++) {
-    if (!Array.isArray(arr[index])) result.push(arr[index]);
-    else {
-      for (let inner = 0; inner < arr[index].length; inner++){
-        result.push(arr[index][inner]);
-      }
+    if (!Array.isArray(arr[index])) {
+      result.push(arr[index]);
+    } else {
+      // for (let inner = 0; inner < arr[index].length; inner++) {
+      //   result.push(arr[index][inner]);
+      // }
+      result = result.concat(flatten(arr[index]));
     }
   }
 
   return result;
 };
 
-assertArraysEqual(flatten([1, 2, [3, 4], [5,6], [7]]), [1, 2, 3, 4, 5, 6, 7]);
+assertArraysEqual(flatten([1, 2, [3, [4,10,11]], [5,6], [7]]), [1, 2, 3, 4, 10, 11, 5, 6, 7]);
 assertArraysEqual(flatten([1, 2]), [1, 2]);
 assertArraysEqual(flatten([[1, 2]]), [1, 2]);
 assertArraysEqual(flatten([[3, 4], [5,6]]), [3, 4, 5, 6]);
