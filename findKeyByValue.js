93a55ea40e144e5b36a4cedccd8ba18bdfa84f8d
t5krishn <tk95@live.ca>
2019-06-26 13:44:06 +0000

    Created and linted findKeyByValue.js, first use of module export to bring in assertEqual

---
+++ findKeyByValue.js
@@ -0,0 +1,43 @@
+const assertEqual = require('./assertEqual.js').assertEqual;
+
+const findKeyByValue = function(obj, value) {
+
+  for (const key in obj) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+
+  return undefined;
+};
+
+
+
+
+
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const empty = {};
+
+assertEqual(findKeyByValue(empty, "The Wire"), undefined);
+assertEqual(findKeyByValue(empty, ""), undefined);
+
+
+const identical = {
+  a : "abc",
+  b : "abc",
+  c : "cba"
+};
+
+assertEqual(findKeyByValue(identical, "abc"), "a");
+assertEqual(findKeyByValue(identical, "cba"), "c");
