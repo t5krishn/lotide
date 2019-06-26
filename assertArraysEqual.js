b53ada6a520577e25323b3f27cf258e029306946
t5krishn <tk95@live.ca>
2019-06-26 17:07:27 +0000

    Added module.exports for eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -21,12 +21,14 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 
+module.exports = {eqArrays};
 
-assertArraysEqual([1, 2, 3], [1, 2]); // => should PASS
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, false, "3"], [1, false, "3"]); // => should PASS
-assertArraysEqual([], []); // => should PASS
-assertArraysEqual([1], [1]); // => should PASS
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
-assertArraysEqual(["ave", "2as", "qwe"], ["ave", "2as", "qwe"]); // => should PASS
-assertArraysEqual(["asd", "cvb", "fdg"], ["ASD", "CVB", "FDG"]); // => should PASS
+
+// assertArraysEqual([1, 2, 3], [1, 2]); // => should PASS
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+// assertArraysEqual([1, false, "3"], [1, false, "3"]); // => should PASS
+// assertArraysEqual([], []); // => should PASS
+// assertArraysEqual([1], [1]); // => should PASS
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
+// assertArraysEqual(["ave", "2as", "qwe"], ["ave", "2as", "qwe"]); // => should PASS
+// assertArraysEqual(["asd", "cvb", "fdg"], ["ASD", "CVB", "FDG"]); // => should PASS
