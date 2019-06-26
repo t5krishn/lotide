7a5bbc41a3f31ddfe224fb4e99283f24d0a0888f
t5krishn <tk95@live.ca>
2019-06-26 12:59:49 +0000

    Created and linted countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,38 @@
+const  assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u274c \u274c \u274c Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const countLetters = function(str) {
+  let noSpace = str.split(' ').join('');
+  const result = {};
+
+  for (const letter of noSpace) {
+    if (result[letter]) {
+      result[letter]++;
+    } else {
+      result[letter] = 1;
+    }
+  }
+
+  return result;
+};
+
+let result1 = countLetters("hello");
+
+assertEqual(result1["h"], 1);
+assertEqual(result1["e"], 1);
+assertEqual(result1["l"], 2);
+assertEqual(result1["o"], 1);
+
+result1 = countLetters("aaaaa bbbbb ee aaa bbb ccc");
+
+assertEqual(result1["a"], 8);
+assertEqual(result1["b"], 8);
+assertEqual(result1["e"], 2);
+assertEqual(result1["c"], 3);
