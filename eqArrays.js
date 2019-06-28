145b8523ede67bbcaf41f35383e1b6012da846ad
t5krishn <tk95@live.ca>
2019-06-28 15:59:36 +0000

    Implemented recurison in eqArrays to compare nested arrays and arrays of objects

--- eqArrays.js
+++ eqArrays.js
@@ -1,23 +1,41 @@
-const  assertEqual = function(actual, expected) {
+const assertEqual = require('./assertEqual.js').assertEqual;
+const eqObjects = require('./eqObjects.js').eqObjects;
 
-  if (actual === expected) {
-    console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u274c \u274c \u274c Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
 
 const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
-
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
   for (let index = 0; index < array1.length; index++) {
-    if (array1[index] !== array2[index]) return false;
+    if (!(typeof array1[index] === typeof array2[index])) {
+      return false;
+    }
+    // console.log("index:", index);
+
+    // Array equal check
+    if (typeof array1[index] === 'object' && Array.isArray(array1[index])) {
+      if (!eqArrays(array1[index], array2[index])) {
+        return false;
+      }
+    // Object equal check
+    } else if (typeof array1[index] === 'object' && !Array.isArray(array1[index])) {
+      if (!eqObjects(array1[index], array2[index])) {
+        return false;
+      }
+    } else {
+      if (array1[index] !== array2[index]) { 
+        return false;
+      }
+    }
   }
 
   return true;
 };
 
+
+
+module.exports = {eqArrays};
 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // => should PASS
@@ -26,5 +44,9 @@ assertEqual(eqArrays([1, false, "3"], [1, false, "3"]), true); // => should PASS
 assertEqual(eqArrays([], []), true); // => should PASS
 assertEqual(eqArrays([1], [1]), true); // => should PASS
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["ave", "2as", "qwe"], ["ave", "2as", "qwe"]), true); // => should PASS
+assertEqual(eqArrays(["ave", [1, 2, 3], "2as", "qwe"], ["ave", [1, 2, 3], "2as", "qwe"]), true); // => should PASS
 assertEqual(eqArrays(["asd", "cvb", "fdg"], ["ASD", "CVB", "FDG"]), false); // => should PASS
+assertEqual(eqArrays([[1,2,3],[[4,[5],[6,[7,8]]]]], [[1,2,3],[[4,[5],[6,[7,8]]]]]), true); // => should PASS
+assertEqual(eqArrays([[1,2,3],[[4,[5],[6,[7,8]]]]], [1,2,3,[[4,[5],[6,[7,8]]]]]), false); // => should PASS
+
+assertEqual(eqArrays([{a:1,b:2}, {c:4, d:3}], [{a:1,b:2}, {c:4, d:3}]), true); // => should PASS
