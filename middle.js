b40a5403b30009a2c27fe6d9cb5030207ae6427f
t5krishn <tk95@live.ca>
2019-07-01 16:32:16 +0000

    Refactored and exported middle

--- middle.js
+++ middle.js
@@ -1,25 +1,4 @@
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
-
-  for (let index = 0; index < array1.length; index++) {
-    if (array1[index] !== array2[index]) return false;
-  }
-
-  return true;
-};
-
-
-const assertArraysEqual = function(array1, array2) {
-  let check = eqArrays(array1, array2);
-
-  if (check) {
-    console.log(`\u2705 \u2705 \u2705 Arrays Equal: ${array1} === ${array2}`);
-  } else {
-    console.log(`\u274c \u274c \u274c Arrays Not Equal: ${array1} !== ${array2}`);
-  }
-};
-
+// const assertArraysEqual = require('./assertArraysEqual.js');
 
 
 const middle = function(arr) {
@@ -35,9 +14,6 @@ const middle = function(arr) {
   }
 };
 
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
+module.exports = middle;
+
+
