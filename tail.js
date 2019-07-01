4989bdfce46ac5e937fe7574d197a67bd2cb9bad
t5krishn <tk95@live.ca>
2019-07-01 15:58:46 +0000

    Refactored and created tailTest.js

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-const  assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u274c \u274c \u274c Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
 
 const tail = function(arr) {
 
@@ -16,13 +7,15 @@ const tail = function(arr) {
 
 };
 
-const result = tail([1,2,3,4]);
-assertEqual(result.length, 3);
-assertEqual(result[0], 2);
-assertEqual(result[1], 3);
-assertEqual(result[2], 4);
+module.exports = tail;
 
-// Checks for modification of array
-const sameArrCheck = [1,2,3,4,5];
-tail(sameArrCheck);
-assertEqual(sameArrCheck.length, 5);
+// const result = tail([1,2,3,4]);
+// assertEqual(result.length, 3);
+// assertEqual(result[0], 2);
+// assertEqual(result[1], 3);
+// assertEqual(result[2], 4);
+
+// // Checks for modification of array
+// const sameArrCheck = [1,2,3,4,5];
+// tail(sameArrCheck);
+// assertEqual(sameArrCheck.length, 5);
